392a01bf1be8f14957ef1ec6543792b571fc375c
Ningluosi <xuzhang1108@163.com>
2023-11-29 23:45:51 +0800

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -31,6 +31,7 @@ public class NBody {
     }
 
     public static void main(String[] args) {
+        double t = 0;
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
@@ -38,13 +39,40 @@ public class NBody {
         Planet[] planets = readPlanets(filename);
         double radius = readRadius(filename);
 
+        StdDraw.enableDoubleBuffering();
         StdDraw.setScale(-radius, radius);
-        StdDraw.clear();
-        StdDraw.picture(0, 0, imageToDraw);
-        StdDraw.show();
 
-        for (Planet p : planets) {
-            p.draw();
+        while (t <= T) {
+            double[] xNetForce = new double[planets.length];
+            double[] yNetForce = new double[planets.length];
+
+            for (int i = 0; i < planets.length; i++) {
+                xNetForce[i] = planets[i].calcNetForceExertedByX(planets);
+                yNetForce[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xNetForce[i], yNetForce[i]);
+            }
+
+            StdDraw.clear();
+            StdDraw.picture(0, 0, imageToDraw);
+            
+            for (Planet p : planets) {
+                p.draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+            t += dt;
+        }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
         }
     }
 }
