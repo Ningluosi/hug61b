b2b60fb2d700da18cf50a5bda56bcbf931bb310a
Ningluosi <xuzhang1108@163.com>
2023-12-20 21:29:25 +0800

    modify ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,5 +1,7 @@
 public class ArrayDeque<T> {
     private T[] items = (T[]) new Object[8];
+    private final int FACTOR = 2;
+    private final double USAGE_FACTOR = 0.25;
     private int size;
     private int nextFirst;
     private int nextLast;
@@ -10,7 +12,29 @@ public class ArrayDeque<T> {
         nextLast = 1;
     }
 
+    public void resize(int newsize) {
+        T[] temp = (T[]) new Object[newsize];
+
+        for (int i = 0; i < size; i++) {
+            temp[i] = items[++nextFirst % items.length];
+        }
+        items = temp;
+        nextFirst = newsize - 1;
+        nextLast = size;
+    }
+
+    public void increaseUsageFactor() {
+        if (items.length >= 16) {
+            double usageFactor = (double)size / (double)items.length;
+            if (usageFactor < USAGE_FACTOR) {
+                resize(size * FACTOR);
+            }
+        }
+    }
     public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * FACTOR);
+        }
         items[nextFirst--] = item;
         if (nextFirst < 0) {
             nextFirst = items.length - 1;
@@ -19,13 +43,16 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * FACTOR);
+        }
         items[nextLast++] = item;
         if (nextLast >= items.length) {
             nextLast = 0;
         }
         size++;
     }
-    
+
     public boolean isEmpty() {
         return size == 0 ? true : false;
     }
@@ -50,8 +77,10 @@ public class ArrayDeque<T> {
         if (size == 0) {
             return null;
         }
+
         T item = items[++nextFirst % items.length];
         size--;
+        increaseUsageFactor();
         return item;
     }
 
@@ -68,6 +97,7 @@ public class ArrayDeque<T> {
         }
 
         size--;
+        increaseUsageFactor();
         return item;
     }
 
