269e55e286b0ff433472887b8644d1d3a6a3a8cb
Ningluosi <xuzhang1108@163.com>
2023-12-30 11:55:13 +0800

    complete TestArrayDequeGold.java

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void test1() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+        String message = "\n";
+
+        for (int i = 0; i < 1000; i += 1) {
+            int numberBetweenZeroAndNine = StdRandom.uniform(16);
+
+            if (numberBetweenZeroAndNine < 4) {
+                sad1.addLast(i);
+                ads1.addLast(i);
+                message = message + "addLast(" + i + ")\n";
+            } else if (numberBetweenZeroAndNine <= 4 && numberBetweenZeroAndNine < 8) {
+                sad1.addFirst(i);
+                ads1.addFirst(i);
+                message = message + "addFirst(" + i + ")\n";
+            } else if (numberBetweenZeroAndNine <= 8 && numberBetweenZeroAndNine < 12) {
+                Integer sadFirst = sad1.removeFirst();
+                Integer adsFirst = ads1.removeFirst();
+                message = message + "removeFirst(): " + sadFirst + "\n";
+                assertEquals(message, sadFirst, adsFirst);
+
+            } else {
+                Integer sadLast = sad1.removeLast();
+                Integer adsLast = ads1.removeLast();
+                message = message + "removeLast(): " + sadLast + "\n";
+                assertEquals(message, sadLast, adsLast);
+            }
+        }
+    }
+}
